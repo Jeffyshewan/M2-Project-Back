94d700ba06b459b8b1aa2d41e35101e15f59228f
Jean-François GAUTREAU <Jeff59550@gmail.com>
2022-03-15 11:47:32 +0100

    :sparkles: : add new models

---
+++ airData/src/main/java/com/project/airData/models/favoriteAirport.java
@@ -0,0 +1,21 @@
+package com.project.airData.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.annotation.Id;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class favoriteAirport {
+
+    @Id
+    public Integer id;
+
+    public String name;
+    public String iata;
+    public String icao;
+    public String country;
+    public String country_iso;
+}
---
+++ airData/src/main/java/com/project/airData/models/flight.java
@@ -0,0 +1,23 @@
+package com.project.airData.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.annotation.Id;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class flight {
+    @Id
+    public Integer id;
+
+    public String flightNumber;
+    public String date;
+    public String departureHour;
+    public String departureAirport;
+    public String departureUtc;
+    public String arrivalHour;
+    public String arrivalAirport;
+    public String arrivalUtc;
+}
